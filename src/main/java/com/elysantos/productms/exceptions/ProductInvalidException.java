fcf4767ac541b91dcc50124b004b19c043858f5a
Ely Santos <ely.franklin@acad.ifma.edu.br>
2021-06-16 15:20:34 -0300

    0.2.1 - mongo db

--- src/main/java/com/elysantos/productms/exceptions/ProductInvalidException.java
+++ src/main/java/com/elysantos/productms/exceptions/ProductInvalidException.java
@@ -1,6 +1,6 @@
 package com.elysantos.productms.exceptions;
 
-public class ProductInvalidException extends Exception {
+public class ProductInvalidException extends RuntimeException {
     public ProductInvalidException(String msg) {
         super(msg);
     }
